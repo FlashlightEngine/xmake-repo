0881d32adcd9d80a27b77d75c3aa56900a904ec7
Jean <jean.letessier@protonmail.com>
2024-08-02 15:04:59 +0200

    Initial commit.

--- packages/t/tree-sitter/port/xmake.lua
+++
@@ -1,8 +0,0 @@
-add_rules("mode.debug", "mode.release")
-target("tree-sitter")
-    set_kind("$(kind)")
-    add_files("lib/src/lib.c")
-    add_includedirs({
-        "lib/src",
-        "lib/include"})
-    add_headerfiles("lib/include/(**.h)")
