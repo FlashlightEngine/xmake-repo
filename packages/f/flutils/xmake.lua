86f1bc87b2bcaa67e1ce24fcc1a1ef4b31dce9b1
Jean Letessier <83421070+Pixfri@users.noreply.github.com>
2024-08-12 15:23:43 +0200

    Fixed FLUtils package.

--- packages/f/flutils/xmake.lua
+++ packages/f/flutils/xmake.lua
@@ -1,10 +1,12 @@
-package("FLUtils")
+package("flutils")
     set_kind("library", {headeronly = true})
     set_homepage("https://github.com/Pixfri/FLUtils")
     set_description("Header-only utilities for FlashlightEngine.")
     set_license("MIT")
 
-    add_urls("1.0.0", "c33f608545d5528a8e288090431051066ed1756a")
+    add_urls("https://github.com/FlashlightEngine/FLUtils.git")
+
+    add_versions("1.0.0", "c33f608545d5528a8e288090431051066ed1756a")
 
     set_policy("package.strict_compatibility", true)
 
