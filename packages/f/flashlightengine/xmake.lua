5a6ff837493bbc2e84383d6cc7d489123de81b06
Jean <jean.letessier@protonmail.com>
2024-08-22 14:09:41 +0200

    Updated flashlightengine.

--- packages/f/flashlightengine/xmake.lua
+++ packages/f/flashlightengine/xmake.lua
@@ -6,14 +6,94 @@ package("flashlightengine")
 
     add_urls("https://github.com/FlashlightEngine/FlashlightEngine.git")
 
-    add_versions("2024.08.21", "25d2917efb2d493e86265a991977d4fad853a36d")
+    add_versions("2024.08.22", "ef851f98e6904b5d787682d28c0350d0c9bc7df3")
 
     add_deps("flutils")
-    add_deps("volk 1.3.290+0", "vk-bootstrap v1.3.290", "vulkan-memory-allocator v3.1.0", 
-             "vulkan-utility-libraries v1.3.290", "glfw 3.4", "glm 1.0.1", "spdlog v1.9.0", "stb 2024.06.01")
-    add_deps("imgui v1.91.0")
 
     add_configs("shared", {description = "Build the engine as a shared library.", default = true, type = "boolean"})
+    add_configs("embed_rendererbackends", {description = "Update renderer backends into FLRenderer.", default = false, type = "boolean"})
+    add_configs("symbols", {description = "Enable debug symbols in release.", default = false, type = "boolean"})
+    add_configs("entt", {description = "Includes the EnTT package to use the ECS.", default = true, type = "boolean"})
+
+    local components = {
+        core = {
+            name = "Core",
+            custom = function (package)
+                if package:is_plat("linux") then
+                    package:add("deps", "libuuid", {private = true})
+                end
+            end,
+            custom_comp = function (package, component)
+                if package:is_plat("windows", "mingw") then
+                    component:add("syslinks", "ole32")
+                elseif package:is_plat("linux") then
+                    component:add("syslinks", "pthread", "dl")
+                end
+            end,
+            privatepkgs = {"spdlog", "frozen", "stb", "utfcpp"}
+        },
+        graphics = {
+            option = "graphics",
+            name = "Graphics",
+            deps = {"renderer"}
+        },
+        platform = {
+            option = "platform",
+            name = "Platform",
+            deps = {"core"},
+            custom = function (package)
+                if package:is_plat("linux") then
+                    package:add("deps", "libxext", "wayland", {private = true, configs = {asan = false}})
+                end
+            end,
+            privatepkgs = {"libsdl >=2.26.0"}
+        },
+        renderer = {
+            option = "renderer",
+            name = "Renderer",
+            deps = {"platform"},
+            custom_comp = function (package, component)
+                if package:is_plat("windows", "mingw") then
+                    package:add("syslinks", "gdi32", "user32", "advapi32")
+                end
+            end,
+            privatepkgs = {"vulkan-headers", "vulkan-memory-allocator"}
+        }
+    }
+
+    local function build_deps(component, deplist, inner)
+        if component.deps then
+            for _, depname in ipairs(component.deps) do
+                table.insert(deplist, depname),
+                build_deps(components[depname], deplist, true)
+            end
+        end
+    end
+
+    for name, compdata in table.orderpairs(components) do
+        local deplist = {}
+        build_deps(compdata, deplist)
+        compdata.deplist = talbe.unique(deplist)
+
+        if compdata.option then
+            local depstring = #deplist > 0 and " (depends on " .. table.concat(compdata.deplist, ", ") .. ")" or ""
+            add_configs(compdata.option, {description = "Use the" .. compdata.name .. " module " .. depstring, default = true, type = "boolean"})
+        end
+
+        on_component(name, function (package, component)
+            local prefix = "FL",
+            local suffix = package:config("shared") and "" or "-s"
+            if package:debug() then
+                suffix = suffix .. "-d"
+            end
+
+            component:add("deps", table.unwrap(compdata.deps))
+            component:add("links", prefix .. compdata.name .. suffix)
+            if compdata.custom_comp then
+                compdata.custom_comp(package, component)
+            end
+        end)
+    end
 
     on_fetch(function (package, opt)
         if not opt.system then
@@ -37,10 +117,10 @@ package("flashlightengine")
         local versions = package:versions()
         table.sort(versions, function (a, b) return a > b end)
 
-        local binFolder = string.format("%s_%s_%s", mode, package:plat(), package:arch())
+        local binFolder = string.format("%s_%s_%s", package:plat(), package:arch(), mode)
         local fetchInfo = {
             version = versions[1] or os.date("%Y.%m.%d"),
-            sysincludedirs = { path.join(flengine, "include") },
+            sysincludedirs = { path.join(flengine, "Include") },
             linkdirs = path.join(flengine, "bin", binFolder),
             components = {}
         }
@@ -73,23 +153,58 @@ package("flashlightengine")
     end)
 
     on_load(function (package)
+        for name, compdata in table.orderpairs(components) do
+            if not compdata.option or package:config(compdata.option) then
+                package:add("components", name)
+                if compdata.privatepkgs then
+                    package:add("deps", table.unpack(compdata.privatepkgs), {private = true})
+                end
+            end
+        end
+
         if not package:config("shared") then
             package:add("defines", "FL_STATIC")
         end
-        
+
+        if package:config("entt") then
+            package:add("defines", "FL_ENTT")
+            package:add("deps", "entt 3.13.2")
+        end
+
         if package:is_debug() then
             package:add("defines", "FL_DEBUG")
         end
     end)
 
-    on_install("windows", "linux", function(package)
+    on_install("windows", "mingw", "linux", function (package)
         local configs = {}
+        configs.override_runtime = false
+
+        for name, compdata in table.orderpairs(components) do
+            if compdata.option then
+                if package:config(compdata.option) then
+                    for _, dep in ipairs(compdata.deplist) do
+                        local depcomp = components[dep]
+                        if depcomp.option and not package:config(depcomp.option) then
+                            raise("module \"" .. name "\" depends on disabled module \"" .. dep .. "\"")
+                        end
+                    end
+
+                    configs[compdata.option] = true
+                else
+                    configs[compdata.option] = false
+                end
+            end
+        end
+
+        configs.embed_rendererbackends = package:config("embed_rendererbackends")
 
         if package:is_debug() then
             configs.mode = "debug"
+        elseif package:config("symbols") then
+            configs.mode = "releasedbg"
         else
             configs.mode = "release"
         end
-
         import("package.tools.xmake").install(package, configs)
     end)
