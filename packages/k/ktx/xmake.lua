f015556c99e3ec0f413b4cf2c53985c090e8ab24
Jean <jean.letessier@protonmail.com>
2024-08-29 23:37:16 +0200

    Added KTX.

---
+++ packages/k/ktx/xmake.lua
@@ -0,0 +1,25 @@
+package("ktx")
+    set_homepage("https://github.com/KhronosGroup/KTX-Software/")
+    set_description("The Khronos KTX library and tools.")
+    
+    add_urls("https://github.com/KhronosGroup/KTX-Software/archive/refs/tags/$(version).tar.gz", 
+             "https://github.com/KhronosGroup/KTX-Software.git")
+
+    add_versions("v4.3.2", "91ace88675ac59a97e55d0378a6602a9ae6b98bd")
+
+    add_deps("cmake")
+
+    add_configs("ktx1", {description = "Enable KTX 1 support.", default = true, type = "boolean"})
+    add_configs("ktx2", {description = "Enable KTX 2 support.", default = true, type = "boolean"})
+    add_configs("vk-upload", {description = "Enable Vulkan texture upload.", default = true, type = "boolean"})
+    add_configs("gl-upload", {description = "Enable OpenGL texture upload.", default = true, type = "boolean"})
+
+    on_install(function (package)
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DKTX_FEATURE_KTX1=" .. (package:config("ktx1") and "ON" or "OFF"))
+        table.insert(configs, "-DKTX_FEATURE_KTX2=" .. (package:config("ktx2") and "ON" or "OFF"))
+        table.insert(configs, "-DKTX_FEATURE_VK_UPLOAD=" .. (package:config("vk-upload") and "ON" or "OFF"))
+        table.insert(configs, "-DKTX_FEATURE_GL_UPLOAD=" .. (package:config("gl-upload") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+    )
