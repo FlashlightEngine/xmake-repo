6c8bf7e0237cdbc7b7f5e205eb6af8f7ee4135d7
Jean <jean.letessier@protonmail.com>
2024-09-14 18:22:02 +0200

    Removed sanitizers.

--- packages/f/flashlightengine/xmake.lua
+++ packages/f/flashlightengine/xmake.lua
@@ -14,36 +14,6 @@ package("flashlightengine")
     add_configs("symbols", {description = "Enable debug symbols in release.", default = false, type = "boolean"})
     add_configs("optick", {description = "Enable the optick debugger.", default = false, type = "boolean"})
 
-    local sanitizers = {
-        asan = "address",
-        lsan = "leak",
-        tsan = "thread"
-    }
-    
-    -- Asan
-    add_configs("asan", {description = "Enable address sanitizer", default = false, type = "boolean"})
-
-    if has_config("asan") then
-        add_defines("FL_WITH_ASAN")
-        set_policy("build.sanitizer.asan", true)
-    end
-    
-    -- Lsan
-    add_configs("lsan", {description = "Enable leak sanitizer", default = false, type = "boolean"})
-
-    if has_config("asan") then
-        add_defines("FL_WITH_LSAN")
-        set_policy("build.sanitizer.lsan", true)
-    end
-    
-    -- Tsan
-    add_configs("tsan", {description = "Enable thread sanitizer", default = false, type = "boolean"})
-
-    if has_config("asan") then
-        add_defines("FL_WITH_TSAN")
-        set_policy("build.sanitizer.tsan", true)
-    end
-
     on_install("windows", "mingw", "linux", function (package)
         local configs = {}
         configs.override_runtime = false
@@ -62,18 +32,6 @@ package("flashlightengine")
             configs.mode = "releasedbg"
         else
             configs.mode = "release"
-        end
-
-        if package:config("asan") then
-            configs.asan = true
-        end
-
-        if package:config("tsan") then
-            configs.tsan = true
-        end
-
-        if package:config("lsan") then
-            configs.lsan = true
         end
 
         import("package.tools.xmake").install(package, configs)
